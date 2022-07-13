50332d3efa4b7c4b35ddda5f549a56cd6725fb3a
junghaekyun <hkj1043@gmail.com>
2022-07-13 09:38:41 +0900

    캡슐화 구현

--- src/CarTest.java
+++ src/CarTest.java
@@ -1,40 +1,26 @@
 public class CarTest {
     public static void main(String[] args) {
-        Car tesla = new Car();
-        Car audi = new Car();
-        Car bmw = new Car();
-        tesla.model = "tesla";
-        tesla.color = "blue";
-        audi.model = "audi";
-        audi.color = "black";
-        bmw.model = "bmw";
-        bmw.color = "white";
-
-        System.out.println("1번 차는 "+tesla.model+"이고 색상은"+tesla.color+"색 입니다.");
-        System.out.println("2번 차는 "+audi.model+"이고 색상은"+audi.color+"색 입니다.");
-        System.out.println("3번 차는 "+bmw.model+"이고 색상은"+bmw.color+"색 입니다.");
-
-        tesla.power();
-        tesla.accelerate();
-        tesla.stop();
-
-//        audi.power();
-//        audi.accelerate();
-//        audi.stop();
-//
-//        bmw.power();
-//        bmw.accelerate();
-//        bmw.stop();
+        Car car = new Car("Model X", "빨간색", 250);
+        System.out.println("제 차는 " + car.getModelName() + "이고, 컬러는 " +  car.getColor() + "입니다.");
     }
 }
-class Car{
-    public String model;
-    public String color;
-    void power(){System.out.println("시동");}
-    void accelerate(){
-        System.out.println("가속");
+
+class Car {
+    private String modelName;
+    private String color;
+    private int maxSpeed;
+
+    public Car(String modelName, String color, int maxSpeed) {
+        this.modelName = modelName;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
     }
-    void stop(){
-        System.out.println("정지");
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
